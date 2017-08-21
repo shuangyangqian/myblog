c941cda8a13f68dd937b5a0a83b4291c15ee8647
shuangyangqian <shuangyang.qian@easystack.cn>
2017-08-21 10:21:49 +0800

    Change the property of the Article.
    Change the property of the Article from context to content.

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class Article(models.Model):
     title = models.CharField(max_length=32, default="Title")
-    context = models.TextField(null=True)
+    content = models.TextField(null=True)
 
     def __unicode__(self):
         return self.title
